0069520cd2be7a9fdf436e989f2c05c54c45dbff
kylemocode <kylemo860617@gmail.com>
2024-12-21 13:17:30 +0800

    chore: apply PR suggestion

--- nextjs-app/app/robots.ts
+++ nextjs-app/app/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl =
-    process.env.NEXT_PUBLIC_BASE_URL ?? "https://https://thementorship.tw/";
+    process.env.NEXT_PUBLIC_BASE_URL ?? "https://thementorship.tw";
 
   return {
     rules: [
