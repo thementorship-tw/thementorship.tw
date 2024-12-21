730121d1954bd1eb7a7e074b95f798be5f6acbad
ChengYiLin <cheng.yi.lin.217@gmail.com>
2024-12-21 10:56:46 +0800

    feat(tailwind): Define custom screen breakpoints for mobile-first development

--- nextjs-app/tailwind.config.ts
+++ nextjs-app/tailwind.config.ts
@@ -4,6 +4,11 @@ import typography from "@tailwindcss/typography";
 export default {
   content: ["./app/**/*.{ts,tsx}", "./sanity/**/*.{ts,tsx}"],
   theme: {
+    screens: {
+      md: { min: "600px" },
+      lg: { min: "840px" },
+      xl: { min: "1280px" },
+    },
     extend: {},
   },
   future: {
