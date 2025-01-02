07d259e8eb1adc40f9a47f83f80045269343bd4e
kylemocode <kylemo860617@gmail.com>
2025-01-02 10:02:13 +0800

    chore: keep webpack setting to prevent build error

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -12,6 +12,13 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   env: {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: "false",
