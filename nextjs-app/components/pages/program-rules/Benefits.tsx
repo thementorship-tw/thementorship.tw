48822c780ee84258ae53e05cfe00744d104f7017
kylemocode <kylemo860617@gmail.com>
2025-01-14 22:38:59 +0800

    feat: finish benifit section

--- nextjs-app/components/pages/program-rules/Benefits.tsx
+++ nextjs-app/components/pages/program-rules/Benefits.tsx
@@ -11,26 +11,27 @@ interface IBenefitCardProps {
 const benefitsData: IBenefitCardProps[] = [
   {
     serial: "01",
-    title: "專業導師指導",
-    description: "一對一專業導師指導，幫助你更快成長",
+    title: "主題月會",
+    description: "和不同職能的產品設計夥伴共創，在有限時間內得到共識並產出成果",
     imageUrl: "/images/icon-hat.png",
   },
   {
     serial: "02",
-    title: "社群交流",
-    description: "與志同道合的夥伴一起學習與成長",
+    title: "主題講座",
+    description:
+      "邀請到深耕產品設計業界的多年的講師們來分享主題工作坊、主題講座、主題對談等來幫助參賽者獲得更多靈",
     imageUrl: "/images/icon-license.png",
   },
   {
     serial: "03",
-    title: "實戰項目",
-    description: "參與真實項目開發，累積實戰經驗",
+    title: "跨職能＆跨界交流",
+    description: "從研究一路到測試、最終提案，並結合企業主題解決真實性問題",
     imageUrl: "/images/icon-sailor-hat.png",
   },
   {
     serial: "04",
-    title: "資源共享",
-    description: "獲得豐富的學習資源與工具",
+    title: "曼陀號社群互動",
+    description: "從研究一路到測試、最終提案，並結合企業主題解決真實性問題",
     imageUrl: "/images/icon-lighthouse.png",
   },
 ];
@@ -42,15 +43,11 @@ const BenefitCard = ({
   imageUrl,
 }: IBenefitCardProps) => {
   return (
-    <div className="bg-white rounded-lg relative">
-      {/* 序號標籤 - 調整大小和位置 */}
+    <div className="bg-white rounded-3 relative h-full flex flex-col">
       <div className="absolute text-h5 -top-5 -left-[13px] bg-yellow-6 text-white w-[52px] h-[52px] flex items-center justify-center rounded-2">
         {serial}
       </div>
-
-      {/* 內容垂直置中 */}
-      <div className="flex flex-col items-center justify-center text-center pt-8 px-7">
-        {/* 圖片 */}
+      <div className="flex flex-col items-center h-full text-center py-8 px-7">
         <Image
           src={imageUrl}
           alt={title}
@@ -58,9 +55,7 @@ const BenefitCard = ({
           height={80}
           className="w-[80px] h-[80px] object-cover mb-6"
         />
-
-        {/* 文字內容 */}
-        <h3 className="text-xl font-bold mb-2">{title}</h3>
+        <h3 className="text-xl font-bold mb-3">{title}</h3>
         <p className="text-gray-600">{description}</p>
       </div>
     </div>
@@ -78,7 +73,7 @@ const Benefits = () => {
         serial="01"
         variant="dark"
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 mt-11">
         {benefitsData.map((benefit) => (
           <BenefitCard key={benefit.serial} {...benefit} />
         ))}
