6c2e25e1aa1083010532dc635fb8b777e28275c3
kylemocode <kylemo860617@gmail.com>
2025-01-16 16:15:30 +0800

    chore: add container class to some section

--- nextjs-app/components/pages/main/Partners.tsx
+++ nextjs-app/components/pages/main/Partners.tsx
@@ -46,42 +46,44 @@ const programPartners = [
 
 const Partners = () => {
   return (
-    <section className="py-[72px] bg-neutral-1 md:py-[120px]">
-      <div className="mx-auto text-center">
-        <h2 className="text-h5 md:text-h4 text-yellow-6 mb-2">合作夥伴</h2>
-        <h3 className="text-h3-title md:text-h1-title font-eb-garamond mb-6 md:mb-8 text-blue-8">
-          Our Partners
-        </h3>
-        <Image
-          src="/images/title-symbol-line.png"
-          alt="title-symbol-line"
-          width={57}
-          height={5}
-          className="mx-auto mb-11"
-        />
-        <div className="w-full px-5 md:px-10 xl:px-[142px]">
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-7">
-            {programPartners.map((partner, idx) => (
-              <a
-                key={idx}
-                href={partner.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-white rounded-3"
-              >
-                <Image
-                  src={partner.imageSrc}
-                  alt={`${partner.name} logo`}
-                  width={324}
-                  height={108}
-                  className="object-contain h-[108px] w-full"
-                />
-              </a>
-            ))}
+    <div className="w-full bg-neutral-1">
+      <section className="container py-[72px] bg-neutral-1 md:py-[120px]">
+        <div className="mx-auto text-center">
+          <h2 className="text-h5 md:text-h4 text-yellow-6 mb-2">合作夥伴</h2>
+          <h3 className="text-h3-title md:text-h1-title font-eb-garamond mb-6 md:mb-8 text-blue-8">
+            Our Partners
+          </h3>
+          <Image
+            src="/images/title-symbol-line.png"
+            alt="title-symbol-line"
+            width={57}
+            height={5}
+            className="mx-auto mb-11"
+          />
+          <div className="w-full px-5 md:px-10 xl:px-[142px]">
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-7">
+              {programPartners.map((partner, idx) => (
+                <a
+                  key={idx}
+                  href={partner.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-white rounded-3"
+                >
+                  <Image
+                    src={partner.imageSrc}
+                    alt={`${partner.name} logo`}
+                    width={324}
+                    height={108}
+                    className="object-contain h-[108px] w-full"
+                  />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
