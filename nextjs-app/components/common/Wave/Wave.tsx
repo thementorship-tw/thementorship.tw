dd64197630b1377d902cbb0402fd34cd37cc9784
ChengYiLin <cheng.yi.lin.217@gmail.com>
2025-01-28 09:39:36 +0800

    feat: Add Wave component with color variants and SVG assets

---
+++ nextjs-app/components/common/Wave/Wave.tsx
@@ -0,0 +1,30 @@
+import { cva } from "class-variance-authority";
+import { FC } from "react";
+import { twMerge } from "tailwind-merge";
+
+const waveClasses = cva("absolute inset-0 w-full h-9", {
+  variants: {
+    color: {
+      yellow: "bg-[url(/images/wave/wave-yellow.svg)]",
+      blue1: "bg-[url(/images/wave/wave-blue1.svg)]",
+      blue7: "bg-[url(/images/wave/wave-blue7.svg)]",
+      white: "bg-[url(/images/wave/wave-white.svg)]",
+      neutral: "bg-[url(/images/wave/wave-neutral.svg)]",
+    },
+  },
+});
+
+const Wave: FC<{
+  color: "yellow" | "blue7" | "blue1" | "white" | "neutral";
+}> = ({ color }) => {
+  return (
+    <div
+      className={twMerge(
+        waveClasses({ color }),
+        "absolute left-0 h-[9px] w-full top-0 translate-y-[-9px]"
+      )}
+    ></div>
+  );
+};
+
+export default Wave;
