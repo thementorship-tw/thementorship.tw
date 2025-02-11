d537fb92705b447d93ad91a804abd8474c35af9f
kylemocode <kylemo860617@gmail.com>
2025-02-11 18:46:30 +0800

    feat: add fable & junyi as partner

--- nextjs-app/constants/partners.ts
+++ nextjs-app/constants/partners.ts
@@ -2,3 +2,5 @@ export const ZEABUR_PARTNER_URL = "https://zeabur.com";
 export const SANGUINE_PARTNER_URL = "https://www.sanguine.com.tw";
 export const POPOP_TAIPEI_PARTNER_URL = "https://popoptaipei.com/";
 export const GIS_GROUP_PARTNER_URL = "https://www.gisgroup.com/";
+export const JUNYI_PARTNER_URL = "https://www.junyiacademy.org/";
+export const FABLE_PARTNER_URL = "https://www.fable.com.tw/";
