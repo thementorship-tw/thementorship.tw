6bf7b80816df4597e2f3f5323906aa36fc785d8c
ChengYiLin <cheng.yi.lin.217@gmail.com>
2025-03-17 03:57:03 +0800

    feat: add new team members and their profiles to the execution group

--- nextjs-app/constants/pages/team.ts
+++ nextjs-app/constants/pages/team.ts
@@ -21,6 +21,18 @@ import HarperAvatar from "@/public/images/execution-group/Harper.jpg";
 import MartinAvatar from "@/public/images/execution-group/Martin.jpg";
 import LizzyAvatar from "@/public/images/execution-group/Lizzy.jpg";
 import KevinAvatar from "@/public/images/execution-group/Kevin.jpg";
+import ChristineAvatar from "@/public/images/execution-group/Christine.jpg";
+import LucasAvatar from "@/public/images/execution-group/Lucas.jpg";
+import GallonAvatar from "@/public/images/execution-group/Gallon.jpg";
+import SandraAvatar from "@/public/images/execution-group/Sandra.jpg";
+import ChiChiAvatar from "@/public/images/execution-group/ChiChi.jpg";
+import CherylAvatar from "@/public/images/execution-group/Cheryl.jpg";
+import GloriaAvatar from "@/public/images/execution-group/Gloria.jpg";
+import NiuAvatar from "@/public/images/execution-group/Niu.jpg";
+import EmilyAvatar from "@/public/images/execution-group/Emily.jpg";
+import JulianAvatar from "@/public/images/execution-group/Julien.jpg";
+import LukeAvatar from "@/public/images/execution-group/Luke.jpg";
+import ZonnieAvatar from "@/public/images/execution-group/Zonnie.jpg";
 
 import { roleDisplayTextMap } from "../role-display-text-map";
 
@@ -234,6 +246,103 @@ export const EXECUTION_GROUP: Record<ExecutionGroupType, IProfileInfo[]> = {
       subTitle: ["第五屆 Data 水手"],
       quote: "曼陀號將會帶給各位意想不到的航程",
       imageUrl: KevinAvatar.src,
+    },
+    {
+      team: Team.BD,
+      name: "王玟樺 Christine Wang",
+      title: "第七屆 BD組 助教",
+      subTitle: ["第四屆 BD 水手"],
+      quote:
+        "沒有做不到，只有想不到。人生要有登上諾曼第的野心；也要有挺過滑鐵盧的勇氣。",
+      imageUrl: ChristineAvatar.src,
+    },
+    {
+      team: Team.BD,
+      name: "周經倫 Lucas Chou",
+      title: "第七屆 BD組 助教",
+      subTitle: ["第六屆 BD 水手"],
+      quote: "能否骰到六，繫於機緣及命運；如何擊中球，全憑膽識與耕耘。",
+      imageUrl: LucasAvatar.src,
+    },
+    {
+      team: Team.DATA,
+      name: "施佳綸 Gallon Shih",
+      title: "第七屆 Data組 助教",
+      subTitle: ["第六屆 Data 航海士"],
+      quote: "認真且快樂過好每一天，珍惜每一次的相遇。",
+      imageUrl: GallonAvatar.src,
+    },
+    {
+      team: Team.DATA,
+      name: "吳怡慧 Sandra Wu",
+      title: "第七屆 Data組 助教",
+      subTitle: ["第六屆 Data 水手"],
+      quote: "一起徜徉於數據之海嗎？",
+      imageUrl: SandraAvatar.src,
+    },
+    {
+      team: Team.ENGINEER,
+      name: "董元琪 Chichi Tung",
+      title: "第七屆 Engineer組 助教",
+      subTitle: ["第六屆 Engineer 水手"],
+      quote: "思路決定出路，格局決定結局。",
+      imageUrl: ChiChiAvatar.src,
+    },
+    {
+      team: Team.ENGINEER,
+      name: "邱思蓉 Cheryl Chiu",
+      title: "第七屆 Engineer組 助教",
+      subTitle: ["第六屆 Engineer 水手"],
+      quote: "在自己的時區中，一切準時。",
+      imageUrl: CherylAvatar.src,
+    },
+    {
+      team: Team.MKT,
+      name: "林靖容 Gloria Lin",
+      title: "第七屆 MKT組 助教",
+      subTitle: ["第六屆 MKT 水手"],
+      quote: "在人生的米字路口，學會當隻靈活的八爪章魚！",
+      imageUrl: GloriaAvatar.src,
+    },
+    {
+      team: Team.MKT,
+      name: "蘇紐 Niu Su",
+      title: "第七屆 MKT組 助教",
+      subTitle: ["第五屆 MKT 水手"],
+      quote: "享受當下，追求結果。",
+      imageUrl: NiuAvatar.src,
+    },
+    {
+      team: Team.PM,
+      name: "陳槿蓉 Emily Chen",
+      title: "第七屆 PM組 助教",
+      subTitle: ["第五屆 PM 水手"],
+      quote: "成長的秘訣就是多快速嘗試、快速失敗。",
+      imageUrl: EmilyAvatar.src,
+    },
+    {
+      team: Team.PM,
+      name: "陳璿文 Julien Chen",
+      title: "第七屆 PM組 助教",
+      subTitle: ["第六屆 PM 航海士"],
+      quote: "I came, I saw, I contributed.",
+      imageUrl: JulianAvatar.src,
+    },
+    {
+      team: Team.UIUX,
+      name: "謝昌達 Luke Hsieh",
+      title: "第七屆 UIUX組 助教",
+      subTitle: ["第三屆 UX 航海士"],
+      quote: "開始跑，離終點就不遠了。",
+      imageUrl: LukeAvatar.src,
+    },
+    {
+      team: Team.UIUX,
+      name: "林依柔 Zonnie Lin",
+      title: "第七屆 UIUX組 助教",
+      subTitle: ["第六屆 UX 水手"],
+      quote: "沒有人可以代替你經歷自己的人生，你的人生體驗由你來設計。",
+      imageUrl: ZonnieAvatar.src,
     },
   ],
 };
