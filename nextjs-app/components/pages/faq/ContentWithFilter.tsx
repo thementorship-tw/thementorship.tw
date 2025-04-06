07c35be1274e0c8a02c23ae62be12beb3045a383
ChengYiLin <cheng.yi.lin.217@gmail.com>
2025-04-06 23:06:01 +0800

    refactor: Centralize FAQ filtering and presentation logic

--- nextjs-app/components/pages/faq/ContentWithFilter.tsx
+++ nextjs-app/components/pages/faq/ContentWithFilter.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { useState, type FC } from "react";
+import { useMemo, useState, type FC } from "react";
 import TagFilter from "@/components/common/TagFilter/TagFilter";
 import FAQItem from "@/components/common/FAQItem";
 import { FAQType, IFilterOption } from "@/types/filter-option";
 import type { AllFAQItemsQueryResult } from "@/sanity.types";
 
 type FilterOptionType = FAQType | "all";
+
+interface IFAQGroup {
+  key: string;
+  name: string;
+  faqList: AllFAQItemsQueryResult;
+}
 
 interface IProps {
   filterOptions: IFilterOption[];
@@ -16,9 +22,23 @@ interface IProps {
 const ContentWithFilter: FC<IProps> = ({ filterOptions, faqItemTypeMap }) => {
   const [selectedFilter, setSelectedFilter] = useState<FilterOptionType>("all");
 
-  const filteredOptions = filterOptions.filter(({ key }) =>
-    selectedFilter === "all" ? key !== "all" : key === selectedFilter
-  );
+  const faqGroupList = useMemo(() => {
+    const filterTypeList =
+      selectedFilter !== "all"
+        ? filterOptions.filter(({ key }) => key === selectedFilter)
+        : filterOptions.filter(({ key }) => key !== "all");
+
+    return filterTypeList.reduce<IFAQGroup[]>((acc, { key, name }) => {
+      const faqList = faqItemTypeMap[key as FAQType];
+      const faqGroup = {
+        key,
+        name,
+        faqList,
+      };
+
+      return acc.concat(faqGroup);
+    }, []);
+  }, [selectedFilter, filterOptions, faqItemTypeMap]);
 
   const handleSelect = (selectedOption: FilterOptionType) => {
     setSelectedFilter(selectedOption);
@@ -35,25 +55,21 @@ const ContentWithFilter: FC<IProps> = ({ filterOptions, faqItemTypeMap }) => {
       />
 
       <div className="flex flex-col gap-10">
-        {filteredOptions.map(({ key, name }) => {
-          const faqList = faqItemTypeMap[key as FAQType];
-
-          return (
-            <div key={key}>
-              <div className="mb-9 relative after:content-[''] after:absolute after:bottom-0 after:rounded-2 after:block after:w-[260px] after:h-[3px] after:bg-yellow-6">
-                <p className="py-4 text-h3 text-blue-8 border-b-[1px] border-neutral-2">
-                  {name}
-                </p>
-              </div>
-
-              <div className="flex flex-col gap-5">
-                {faqList?.map(({ question, answer }) => (
-                  <FAQItem key={question} question={question} answer={answer} />
-                ))}
-              </div>
+        {faqGroupList.map(({ key, name, faqList }) => (
+          <div key={key}>
+            <div className="mb-9 relative after:content-[''] after:absolute after:bottom-0 after:rounded-2 after:block after:w-[260px] after:h-[3px] after:bg-yellow-6">
+              <p className="py-4 text-h3 text-blue-8 border-b-[1px] border-neutral-2">
+                {name}
+              </p>
             </div>
-          );
-        })}
+
+            <div className="flex flex-col gap-5">
+              {faqList?.map(({ question, answer }) => (
+                <FAQItem key={question} question={question} answer={answer} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
