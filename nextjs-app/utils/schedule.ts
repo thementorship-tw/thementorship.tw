07935dd9a9be48f87fb7324a9c5f1a224d583783
Hannah ʕ•ᴥ•ʔ <99162505+hangineer@users.noreply.github.com>
2026-03-31 05:53:40 +0000

    feat: getSchedulePhaseByMont compares month numbers and the year

--- nextjs-app/utils/schedule.ts
+++ nextjs-app/utils/schedule.ts
@@ -1,26 +1,40 @@
 import { SchedulePhase } from "@/types/schedule";
 
-const getCurrentMonthInTaipei = (now: Date): number =>
-  Number(
-    now.toLocaleDateString("en-US", {
-      month: "numeric",
-      timeZone: "Asia/Taipei",
-    })
-  );
+const CURRENT_YEAR = 2026;
+
+const getCurrentDate = (now: Date): {
+  year: number;
+  month: number;
+} => {
+  const date = now.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "numeric",
+    timeZone: "Asia/Taipei",
+  });
+  const [monthString, yearString] = date.split("/");
+  
+  return {
+    year: Number(yearString),
+    month: Number(monthString),
+  };
+};
 
 export const getSchedulePhaseByMonth = (
   {
+    year = CURRENT_YEAR,
     startMonth,
     endMonth,
   }: {
+    year?: number;
     startMonth: number;
     endMonth: number;
   },
 ): SchedulePhase => {
   const now = new Date();
-  const currentMonth = getCurrentMonthInTaipei(now);
+  const currentDate = getCurrentDate(now);
+  if (currentDate.year > year) return SchedulePhase.EXPIRED;
 
-  if (currentMonth < startMonth) return SchedulePhase.ACTIVE;
-  if (currentMonth > endMonth) return SchedulePhase.EXPIRED;
+  if (currentDate.month < startMonth) return SchedulePhase.ACTIVE;
+  if (currentDate.month > endMonth) return SchedulePhase.EXPIRED;
   return SchedulePhase.ONGOING;
 };
